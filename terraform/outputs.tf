d6e01937b678c445e6386b8ae5ea9d3a9803f213
Daniel O'driscoll <daniel.m.odriscoll@gmail.com>
2026-08-24 21:19:58 +0100

    Added main.tf, provisioned 3 resources with terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "instance_public_ip" {
+  value       = aws_instance.web.public_ip       # the server's public IP address
+  description = "Public IP of the EC2 instance — use this to SSH in"
+}
